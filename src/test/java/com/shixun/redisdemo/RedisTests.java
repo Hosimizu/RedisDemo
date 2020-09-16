fdf08715c9ba609421e5783315d8734d4faaa43d
刘治远 <714775038@qq.com>
2020-09-16 09:47:19 +0800

    发表文章、投票、打印文章信息。

--- src/test/java/com/shixun/redisdemo/RedisTests.java
+++ src/test/java/com/shixun/redisdemo/RedisTests.java
@@ -1,6 +1,7 @@
 package com.shixun.redisdemo;
 
 
+import com.shixun.redisdemo.app.ArticleApp;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,12 +19,26 @@ public class RedisTests {
     @Resource
     private StringRedisTemplate stringRedisTemplate;
 
+    @Resource
+    ArticleApp articleApp;
     @Test
     public void testSet() {
         redisTemplate.opsForValue().set("name1","lzy");
         System.out.println(redisTemplate.opsForValue().get("name1"));
         stringRedisTemplate.opsForValue().set("name2","hosimizu");
         System.out.println(stringRedisTemplate.opsForValue().get("name2"));
+    }
+    @Test
+    public void testAddArticle() {
+        articleApp.addArticle("hosimizu");
+    }
+    @Test
+    public void testVoteArticle() {
+        articleApp.voteArticle(1,"lzy");
+    }
+    @Test
+    public void testArticlesSortedByVote(){
+        articleApp.articlesSortedByVote();
     }
 
 }
